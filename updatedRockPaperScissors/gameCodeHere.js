e8574ec5333457d3d7d2bcc545cb0a920b0cab10
IlerraS <llerra.suerte@gmail.com>
2022-07-22 22:21:57 -0500

    See past messages

--- updatedRockPaperScissors/gameCodeHere.js
+++ updatedRockPaperScissors/gameCodeHere.js
@@ -1,23 +1,83 @@
-let playerChoice;
-let choiceList = ["rock", "paper", "scissors"];
-// let computerChoice = choiceList[Math.floor(Math.random()*3)];
-let counter = 0;
+let winners = [];
 
-
-const playerButtons = document.querySelectorAll('button')
-
-while (counter > 1)
+let StartGame = () =>
 {
-    playerButtons.forEach((button) => 
+    const playerButtons = document.querySelectorAll('.choice')
+
+    playerButtons.forEach((choice) => 
     {
-        button.addEventListener('click', () => 
+        choice.addEventListener('click', function (e)
         {
-            playerChoice = button.id;
-            if (playerChoice == "Paper")
-            {
-                console.log(playerChoice);
-            }
+            console.log(e.target.value);
+            playRound(e.target.value);
         });
     });
-    counter++;
 }
+
+let computerChoose = () =>
+{
+    let choices = ["Rock", "Paper", "Scissors"];
+    let pcChoice = choices[Math.floor(Math.random()*choices.length)];
+    console.log(pcChoice);
+
+    return pcChoice;
+}
+
+let playRound = (playerValue) =>
+{
+    let pcChoice = computerChoose();
+    
+    decideWinner(playerValue, pcChoice);
+
+}
+
+let decideWinner = (userChoice, pcChoice) =>
+{
+    if( userChoice === "Rock")
+        {
+            if(pcChoice === "Rock")
+            {
+                console.log("Tie!");
+            }
+            else if (pcChoice === "Paper")
+            {
+                console.log("AI wins!");
+            }
+            else
+            {
+                console.log("Player wins!");
+            }
+        }
+        else if( userChoice === "Paper")
+        {
+            if(pcChoice === "Rock")
+            {
+                console.log("Player wins!");
+            }
+            else if (pcChoice === "Paper")
+            {
+                console.log("Tie!");
+            }
+            else
+            {
+                console.log("AI wins!");
+            }
+        }
+        else if( userChoice === "Scissors")
+        {
+            if(pcChoice === "Rock")
+            {
+                console.log("AI wins");
+            }
+            else if (pcChoice === "Paper")
+            {
+                console.log("Player wins!");
+            }
+            else
+            {
+                console.log("Tie!");
+            }
+        }
+}
+
+StartGame();
