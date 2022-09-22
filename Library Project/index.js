569588600fd4facd19831017efefbd07f8b7a18d
IlerraS <llerra.suerte@gmail.com>
2022-09-21 20:18:08 -0500

    Started files for Library project

---
+++ Library Project/index.js
@@ -0,0 +1,23 @@
+'use strict';
+class Librarian
+{
+    constructor(author, title, pagesNum, hasBeenRead)
+    {
+        this.author = author;
+        this.title = title;
+        this.pagesNum = pagesNum;
+        this.hasBeenRead = hasBeenRead;
+    }
+    
+    hasBeenRead()
+    {
+        return this._hasbeenread = this.hasBeenRead;
+    }
+}
+
+//
+
+
+
+let myLibrary = [];
+
